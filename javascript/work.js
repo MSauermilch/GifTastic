a949904af549eaf67563dcff5b184e3be69f6cd2
MattSauermilch <madmattschool@gmail.com>
2019-02-11 00:01:46 -0600

    removed commits for clearity working on play/gif feature

--- javascript/work.js
+++ javascript/work.js
@@ -1,22 +1,17 @@
 
-    //The Topics to be searched
-    var topics = [ "lsd","aliens","horror","boogeyman","cartoons","beetlejuice","cult","eyes","weed","tv"]; 
+var topics = [ "lsd","aliens","horror","boogeyman","cartoons","beetlejuice","cult","eyes","weed","tv"]; 
 
-    //creates buttons based on Topics
     function buttGen(){
-      //keeps buttons from the 'ol i++
-      $("#buttonsDiv").empty(); //keeps buttons from i++
+      
+      $("#buttonsDiv").empty();
 
-      //generates buttons from topics list, signs text, class, data.
       for (i=0; i< topics.length; i++) {  
         var theme = topics[i];
         var button = $("<button>");
-        button.html(theme).addClass("btn btn-secondary btn-sm");
-        button.attr("data-item",theme);
+        button.html(theme).addClass("btn btn-secondary btn-sm").attr("data-item",theme);
         $("#buttonsDiv").append(button);
         };
 
-      //button spacing
       $("button").css("margin", "10px");
 
       setClickEvent();
@@ -24,42 +19,47 @@
 
     buttGen(); 
 
-    //sumbit button for html input form
     $("#submitButton").on("click", function() {
-      var newGif = $("#userGif").val().trim(); //.trim()?
+      var newGif = $("#userGif").val().trim(); 
       topics.push(newGif);
       console.log(topics);
 
       buttGen();
     });
 
-    //make compairison so that multible same enteries cant not be made, if so send alert. //Function by Richard
-    function setClickEvent() {                       
+    function setClickEvent() { 
+      console.log("query")                      
     $("button").on("click", function() {
           var item = $(this).attr("data-item");
-          console.log(item);
 
           var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + item + "&api_key=dc6zaTOxFJmzC&limit=10";
 
             $.ajax({
               url: queryURL,
               method: "GET"
-            })
-
-              .then(function(response) {
+            }).then(function(response) {
                 var results = response.data;  
 
-                for (var i = 0; i < results.length; i++) {    
-                  var gifDiv = $("<div>").addClass("gif"); //Creating a div for gif
+                for (var i = 0; i < results.length; i++)
+                {    
+                  var gifDiv = $("<div>").addClass("gif");
                   var rating = results[i].rating;
                   var p = $("<p>").text("Rating: " + rating); //Creates Rating
-                  var personImage = $("<img>");
-                  personImage.attr("src", results[i].images.fixed_height.url);
-                  gifDiv.append(personImage); //Appending Gif
-                  gifDiv.append(p); //Appending Paragraph
-
-                  $("#results").append(gifDiv); // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                  var personImage = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+                                   personImage.attr("data-still-url",response.data[i].images.fixed_height_still.url);
+                                   personImage.attr("data-animated-url",response.data[i].images.fixed_height.url);
+                                   gifDiv.append(personImage).append(p); //Appending Gif
+                  
+                  $("#results").prepend(gifDiv);
                 };
               });
           });
         }
+
+      $("#gif").click( function(){
+        console.log('click click, slit slit');
+        if ($(this).attr("src") === $(this).attr("data-still-url")) {
+            $(this).attr("src", $(this).attr("data-animated-url")) }
+        else if ($(this).attr("src") === $(this).attr("data-animated-url")) {
+            $(this).attr("src", $(this).attr("data-still-url")) }
+      });
