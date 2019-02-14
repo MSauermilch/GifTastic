e75062ea54b7a0a2584d8a15ceadd973901077d5
MattSauermilch <madmattschool@gmail.com>
2019-02-13 20:36:24 -0600

    reworking pause/play

--- javascript/work.js
+++ javascript/work.js
@@ -27,8 +27,7 @@ var topics = [ "lsd","aliens","horror","boogeyman","cartoons","beetlejuice","cul
       buttGen();
     });
 
-    function setClickEvent() { 
-      console.log("query")                      
+    function setClickEvent() {                       
     $("button").on("click", function() {
           var item = $(this).attr("data-item");
 
@@ -45,9 +44,11 @@ var topics = [ "lsd","aliens","horror","boogeyman","cartoons","beetlejuice","cul
                   var gifDiv = $("<div>").addClass("gif");
                   var rating = results[i].rating;
                   var p = $("<p>").text("Rating: " + rating); //Creates Rating
-                  var personImage = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+                  var personImage = $("<img>").addClass("image");
+                                   personImage.attr("src", response.data[i].images.fixed_height_still.url);
                                    personImage.attr("data-still-url",response.data[i].images.fixed_height_still.url);
                                    personImage.attr("data-animated-url",response.data[i].images.fixed_height.url);
+                                   personImage.attr("data-state","still");
                                    gifDiv.append(personImage).append(p); //Appending Gif
                   
                   $("#results").prepend(gifDiv);
@@ -56,10 +57,48 @@ var topics = [ "lsd","aliens","horror","boogeyman","cartoons","beetlejuice","cul
           });
         }
 
-      $("#gif").click( function(){
-        console.log('click click, slit slit');
-        if ($(this).attr("src") === $(this).attr("data-still-url")) {
-            $(this).attr("src", $(this).attr("data-animated-url")) }
-        else if ($(this).attr("src") === $(this).attr("data-animated-url")) {
-            $(this).attr("src", $(this).attr("data-still-url")) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      $(document).on("click", ".image", function(){
+        var imageSelect = $(this).attr("data-state");
+        console.log(this)
+        if (imageSelect === "still") {
+          $(this).attr("data-state", "animate"); 
+          $(this).attr("src", $(this).attr("data-animated-url"));
+        } else {
+          $(this).attr("data-state", "still"); 
+          $(this).attr("src", $(this).attr("data-still-url"));
+        }
       });
+
+
+
+
+
+      //   if ($(this).attr("src") === $(this).attr("data-still-url")) {
+      //       $(this).attr("src", $(this).attr("data-animated-url")) }
+      //   else if ($(this).attr("src") === $(this).attr("data-animated-url")) {
+      //       $(this).attr("src", $(this).attr("data-still-url")) }
+      // });
